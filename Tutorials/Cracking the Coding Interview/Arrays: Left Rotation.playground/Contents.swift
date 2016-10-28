2e5a61422e2a57dc8ddd1e1fc55e3cb00d1c11af
giovannygrability <giovanny.orozco@grability.com>
2016-10-28 11:36:52 -0500

    update 30 days of code

--- Tutorials/Cracking the Coding Interview/Arrays: Left Rotation.playground/Contents.swift
+++ Tutorials/Cracking the Coding Interview/Arrays: Left Rotation.playground/Contents.swift
@@ -1,1 +1,29 @@
+import Foundation
 
+//Hacker rank imputs
+//let inputs = readLine()!.componentsSeparatedByString(" ").map { Int(String($0))! }
+//let count = inputs[0]
+//let torations = inputs[1]
+
+let torations = 4
+var elements = [1,2,3,4,5] //readLine()!.componentsSeparatedByString(" ").map { Int(String($0))! }
+
+for _ in 0 ..< torations {
+    let element = elements[0]
+    elements.removeFirst()
+    elements.append(element)
+}
+
+let string = elements.reduce("") {
+    
+    var string = "\($0) \($1)"
+    
+    //Fix compiler error
+    if string.characters.first == " " {
+        string = "\($1)"
+    }
+    
+    return string
+}
+
+print(string)
