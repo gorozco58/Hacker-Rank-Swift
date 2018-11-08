3fb4963e169585b1fcde5b0c25be7ceb88a789eb
Giovanny Orozco <giovanny.orozco@rappi.com>
2018-11-07 19:33:08 -0500

    Add DP problems

--- DynamicProgramming.playground/Contents.swift
+++ DynamicProgramming.playground/Contents.swift
@@ -22,3 +22,32 @@ func numWaysBottomUp(_ stairs: Int, _ steps: [Int]) -> Int {
 }
 
 numWaysBottomUp(5, [1, 2])
+
+func countSets(_ elements: [Int], total: Int) -> Int {
+    func helper(_ total: Int, _ index: Int, mem: inout [String : Int]) -> Int {
+
+        let key = "\(total):\(index)"
+        var result = 0
+
+        if let result = mem[key] {
+            return result
+        } else if total < elements[index] {
+            result = helper(total, index - 1, mem: &mem)
+        } else {
+            let rest = total - elements[index]
+            let next = index - 1
+
+            func validate(_ value: Int) -> Int {
+                return (value < 0 || next < 0) ? 0 : helper(value, next, mem: &mem)
+            }
+            result = (rest == 0 ? 1 : validate(rest)) + validate(total)
+        }
+
+        mem[key] = result
+        return result
+    }
+    var mem: [String : Int] = [:]
+    return helper(total, elements.count-1, mem: &mem)
+}
+
+countSets([2,4,6,10,12], total: 16)
